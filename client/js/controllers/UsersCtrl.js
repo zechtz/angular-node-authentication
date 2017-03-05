42c69e8c0aa951ba829634a002ae1825a508ebef
Zachariah Ngonyani <zachariahn@gmail.com>
2017-03-05 21:08:02 +0300

    ADd userscontroller to handle clientside users registration

---
+++ client/js/controllers/UsersCtrl.js
@@ -0,0 +1,32 @@
+angular.module('myApp')
+  .controller('UsersCtrl',
+    ['$scope', '$location', 'AuthService',
+      function ($scope, $location, AuthService) {
+
+        $scope.register = function () {
+
+          // initial values
+          $scope.error    =  false;
+          $scope.disabled =  true;
+          $scope.title    =  "Register";
+
+          // call register from service
+          AuthService.register($scope.username, $scope.password)
+          // handle success
+            .then(function (successMessage) {
+              $location.path('/login');
+              $scope.disabled       =  false;
+              $scope.successMessage =  successMessage;
+              $scope.name           =  {};
+              $scope.password       =  {};
+            })
+          // handle error
+            .catch(function (errorMessage) {
+              $scope.error        =  true;
+              $scope.errorMessage =  errorMessage;
+              $scope.disabled     =  false;
+              $scope.name         =  {};
+              $scope.password     =  {};
+            });
+        };
+      }]);
