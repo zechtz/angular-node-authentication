98362c3eb57d65b435cb85a19ac1e2378b9fc678
Zachariah Ngonyani <zachariahn@gmail.com>
2017-03-06 08:36:49 +0300

    clear fields on failed login

--- client/js/controllers/SessionCtrl.js
+++ client/js/controllers/SessionCtrl.js
@@ -1,31 +1,30 @@
-angular.module('myApp')
-  .controller('SessionCtrl', [
-    '$scope', '$location', 'AuthService',
-    function($scope, $location, AuthService){
+angular.module('myApp') .controller('SessionCtrl', [
+  '$scope', '$location', 'AuthService',
+  function($scope, $location, AuthService){
 
-      $scope.login = function() {
-        // initial values
-        $scope.error    =  false;
-        $scope.disabled =  true;
+    $scope.login = function() {
+      // initial values
+      $scope.error    =  false;
+      $scope.disabled =  true;
 
-        AuthService.login($scope.username, $scope.password)
-          .then(function(){
-            $location.path('/');
-            $scope.disabled = false;
-            $scope.loginForm = {};
-          })
-          .catch(() => {
-            $scope.error = true;
-            $scope.errorMessage = 'Invalid username and/or password';
-            $scope.disabled = false;
-            $scope.loginForm = {};
-          });
-      };
+      AuthService.login($scope.username, $scope.password)
+        .then(function(user){
+          $location.path('/');
+          $scope.disabled = false;
+        })
+        .catch(() => {
+          $scope.error = true;
+          $scope.errorMessage = 'Invalid username and/or password';
+          $scope.disabled = false;
+          $scope.username = "";
+          $scope.password = "";
+        });
+    };
 
-      $scope.logout = function () {
-        AuthService.logout()
-          .then(function () {
-            $location.path('/login');
-          });
-      };
-    }]);
+    $scope.logout = function () {
+      AuthService.logout()
+        .then(function () {
+          $location.path('/login');
+        });
+    };
+  }]);
