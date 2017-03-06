897f0044c4e51c78b4ff0773ed3eeb82da84c953
Zachariah Ngonyani <zachariahn@gmail.com>
2017-03-06 08:39:58 +0300

    format service and route index code

--- client/js/service.js
+++ client/js/service.js
@@ -1,89 +1,88 @@
-angular.module('myApp')
-  .factory('AuthService', [
-    '$q', '$timeout', '$http',
-    ($q, $timeout, $http)  => {
-      // create a user variable
-      let user = null;
+angular.module('myApp').factory('AuthService', [
+  '$q', '$timeout', '$http',
+  ($q, $timeout, $http)  => {
+    // create a user variable
+    let user = null;
 
-      function isLoggedIn() {
-        if (user) return true;
-        return false;
-      }
-
-      /**
-       * persist user login status
-       */
-      function getUserStatus() {
-        return $http.get('/user/status')
-          .then(function(data) {
-            if (data.status) {
-              user = true;
-            } else {
-              user = false;
-            }
-          }, function(data) {
-            user = false;
-          });
-      }
-
-      function login(username, password) {
-        // create new instance of deferred
-        let deferred = $q.defer();
-
-        $http.post('/login', {username: username,  password: password })
-          .then(function(data, status){
-            if (data.status === 200 && data.status ){
-              user = true;
-              deferred.resolve();
-            } else {
-              user = false;
-              deferred.reject();
-            }
-          },function(data) {
-            user = false;
-            deferred.reject();
-          });
-        return deferred.promise;
-      }
-
-      function logout() {
-        let deferred = $q.defer();
-        $http.get('/logout')
-          .then(function(data){
-            user = false;
-            deferred.resolve();
-          },function(data){
-            user = false;
-            deferred.reject();
-          });
-        return deferred.promise;
-      }
-
-      function register(username, password) {
-
-        let deferred = $q.defer();
-
-        $http.post('/register', {username: username,  password: password })
-          .then(function(data, status){
-            console.log(username);
-            if (data.status === 200 && data.status){
-              console.log(data.data.status);
-              deferred.resolve(data.data.status);
-            } else {
-              deferred.reject();
-            }
-          },function(data){
-            deferred.reject(data.data.err.message);
-          });
-        return deferred.promise;
-      }
-
-      return ({
-        isLoggedIn    : isLoggedIn,
-        getUserStatus : getUserStatus,
-        login         : login,
-        logout        : logout,
-        register      : register
-      });
+    function isLoggedIn() {
+      if (user) return true;
+      return false;
     }
-  ]);
+
+    /**
+     * persist user login status
+     */
+    function getUserStatus() {
+      return $http.get('/status')
+        .then(function(data) {
+          if (data.status) {
+            user = true;
+          } else {
+            user = false;
+          }
+        }, function(data) {
+          user = false;
+        });
+    }
+
+    function login(username, password) {
+      // create new instance of deferred
+      let deferred = $q.defer();
+
+      $http.post('/login', {username: username,  password: password })
+        .then(function(data, status){
+          if (data.status === 200 && data.status ){
+            user = true;
+            deferred.resolve();
+          } else {
+            user = false;
+            deferred.reject();
+          }
+        },function(data) {
+          user = false;
+          deferred.reject();
+        });
+      return deferred.promise;
+    }
+
+    function logout() {
+      let deferred = $q.defer();
+      $http.get('/logout')
+        .then(function(data){
+          user = false;
+          deferred.resolve();
+        },function(data){
+          user = false;
+          deferred.reject();
+        });
+      return deferred.promise;
+    }
+
+    function register(username, password) {
+
+      let deferred = $q.defer();
+
+      $http.post('/register', {username: username,  password: password })
+        .then(function(data, status){
+          console.log(username);
+          if (data.status === 200 && data.status){
+            console.log(data.data.status);
+            deferred.resolve(data.data.status);
+          } else {
+            deferred.reject();
+          }
+        },function(data){
+          deferred.reject(data.data.err.message);
+        });
+      return deferred.promise;
+    }
+
+    return ({
+      isLoggedIn    : isLoggedIn,
+      getUserStatus : getUserStatus,
+      login         : login,
+      logout        : logout,
+      register      : register
+    });
+  }
+]);
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,15 +4,15 @@ var session  =  require('./session');
 var users    =  require('./user');
 var defaults =  require('./defaults');
 
-router.get('/login',       session.new);
-router.post('/login',      session.create);
-router.get('/user/status', session.status);
-router.get('/logout',      session.destroy);
+router.get('/login',  session.new);
+router.post('/login', session.create);
+router.get('/status', session.status);
+router.get('/logout', session.destroy);
 
-router.get('/users/new',  users.new);
+router.get('/users/new', users.new);
 router.post('/register', users.create);
 
 router.get('/ping', defaults.ping);
-router.get('*', defaults.default);
+router.get('*',     defaults.default);
 
 module.exports = router;
