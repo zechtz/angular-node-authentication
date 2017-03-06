1ca0812eb2ce03021ae671e07b13efe448b9197c
Zachariah Ngonyani <zachariahn@gmail.com>
2017-03-06 08:38:22 +0300

    Format code

--- client/js/main.js
+++ client/js/main.js
@@ -20,31 +20,31 @@ myApp.run(function($rootScope, $location, $route, AuthService) {
 myApp.config(function($routeProvider){
   $routeProvider
     .when('/', {
-      templateUrl: 'partials/home.html',
-      controller: 'MainCtrl',
-      access: {restricted: true}
+      templateUrl : 'partials/home.html',
+      controller  : 'MainCtrl',
+      access      : {restricted: true}
     })
     .when('/login', {
-      templateUrl: 'partials/login.html',
-      controller: 'SessionCtrl',
-      access: {restricted: false}
+      templateUrl : 'partials/login.html',
+      controller  : 'SessionCtrl',
+      access      : {restricted: false}
     })
     .when('/logout', {
-      controller: 'SessionCtrl',
-      access: {restricted: true}
+      controller : 'SessionCtrl',
+      access     : {restricted: true}
     })
     .when('/register', {
-      templateUrl: 'partials/register.html',
-      controller: 'UsersCtrl',
-      access: {restricted: false}
+      templateUrl : 'partials/register.html',
+      controller  : 'UsersCtrl',
+      access      : {restricted: false}
     })
     .when('/one', {
-      template: '<h1> This is page one</h1>',
-      access: {restricted: true}
+      template : '<h1> This is page one</h1>',
+      access   : {restricted: true}
     })
     .when('/two', {
-      template: '<h1> This is page two</h1>',
-      access: {restricted: false}
+      template : '<h1> This is page two</h1>',
+      access   : {restricted: false}
     })
     .otherwise({
       redirectTo: '/'
