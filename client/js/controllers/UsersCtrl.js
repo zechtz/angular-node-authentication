5a7a5ff7ea0ab55a6640c63a5feee81a741b3a06
Zachariah Ngonyani <zachariahn@gmail.com>
2017-03-06 08:37:59 +0300

    Format code

--- client/js/controllers/UsersCtrl.js
+++ client/js/controllers/UsersCtrl.js
@@ -1,32 +1,30 @@
-angular.module('myApp')
-  .controller('UsersCtrl',
-    ['$scope', '$location', 'AuthService',
-      function ($scope, $location, AuthService) {
+angular.module('myApp').controller('UsersCtrl',
+  ['$scope', '$location', 'AuthService', function ($scope, $location, AuthService) {
 
-        $scope.register = function () {
+    $scope.register = function () {
 
-          // initial values
-          $scope.error    =  false;
-          $scope.disabled =  true;
-          $scope.title    =  "Register";
+      // initial values
+      $scope.error    =  false;
+      $scope.disabled =  true;
+      $scope.title    =  "Register";
 
-          // call register from service
-          AuthService.register($scope.username, $scope.password)
-          // handle success
-            .then(function (successMessage) {
-              $location.path('/login');
-              $scope.disabled       =  false;
-              $scope.successMessage =  successMessage;
-              $scope.name           =  {};
-              $scope.password       =  {};
-            })
-          // handle error
-            .catch(function (errorMessage) {
-              $scope.error        =  true;
-              $scope.errorMessage =  errorMessage;
-              $scope.disabled     =  false;
-              $scope.name         =  {};
-              $scope.password     =  {};
-            });
-        };
-      }]);
+      // call register from service
+      AuthService.register($scope.username, $scope.password)
+      // handle success
+        .then(function (successMessage) {
+          $location.path('/login');
+          $scope.disabled       =  false;
+          $scope.successMessage =  successMessage;
+          $scope.username       =  "";
+          $scope.password       =  "";
+        })
+      // handle error
+        .catch(function (errorMessage) {
+          $scope.error        =  true;
+          $scope.errorMessage =  errorMessage;
+          $scope.disabled     =  false;
+          $scope.username     =  "";
+          $scope.password     =  "";
+        });
+    };
+  }]);
